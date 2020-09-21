64c288711cd4f14f5ba6c4a888cbae8510f5e97e
Bohdan Khorolets <b@khorolets.com>
2020-09-21 18:10:55 +0300

    refactor: Upgrade indexer framework version (#7)
    * Upgrade Indexer Framework dependency to 0.2.x version
    
    * Remove outdated code that handles transactions, outcomes, receipts, access_keys and accounts

--- src/models/actions.rs
+++ src/models/actions.rs
@@ -5,7 +5,7 @@ use near_indexer;
 use near_indexer::near_primitives::views::ActionView;
 
 use crate::schema;
-use schema::{actions};
+use schema::actions;
 
 #[derive(Insertable)]
 pub struct Action {
@@ -16,18 +16,18 @@ pub struct Action {
 }
 
 impl Action {
-    pub fn from_action(
-        receipt_id: String,
-        index: i32,
-        action_view: &ActionView
-    ) -> Self {
+    pub fn from_action(receipt_id: String, index: i32, action_view: &ActionView) -> Self {
         let (type_, args): (&str, Option<Value>) = match &action_view {
             ActionView::CreateAccount => ("create_account", None),
-            ActionView::DeployContract { code } => (
-                "deploy_contract",
-                Some(json!({ "code": code }))
-            ),
-            ActionView::FunctionCall { method_name, args, gas, deposit } => (
+            ActionView::DeployContract { code } => {
+                ("deploy_contract", Some(json!({ "code": code })))
+            }
+            ActionView::FunctionCall {
+                method_name,
+                args,
+                gas,
+                deposit,
+            } => (
                 "function_call",
                 Some(json!({
                     "method_name": method_name,
@@ -36,18 +36,20 @@ impl Action {
                     "deposit": deposit.to_string(),
                 })),
             ),
-            ActionView::Transfer { deposit } => (
-                "transfer",
-                Some(json!({ "deposit": deposit.to_string() }))
-            ),
+            ActionView::Transfer { deposit } => {
+                ("transfer", Some(json!({ "deposit": deposit.to_string() })))
+            }
             ActionView::Stake { stake, public_key } => (
                 "stake",
                 Some(json!({
                     "stake": stake.to_string(),
                     "public_key": public_key,
-                }))
+                })),
             ),
-            ActionView::AddKey { public_key, access_key } => (
+            ActionView::AddKey {
+                public_key,
+                access_key,
+            } => (
                 "add_key",
                 Some(json!({
                     "public_key": public_key,
@@ -58,14 +60,14 @@ impl Action {
                 "delete_key",
                 Some(json!({
                     "public_key": public_key,
-                }))
+                })),
             ),
             ActionView::DeleteAccount { beneficiary_id } => (
                 "delete_account",
                 Some(json!({
                     "beneficiary_id": beneficiary_id,
-                }))
-            )
+                })),
+            ),
         };
         Self {
             receipt_id,
