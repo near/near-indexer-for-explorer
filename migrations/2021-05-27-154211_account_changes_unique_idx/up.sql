215de55d0e472b15f73a8789138cd46dfd5c4a12
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2021-06-01 17:35:18 +0300

    fix: unique indexes in account_changes table should contain update_reason
    I guess it's not OK to fix migration scripts, but we never invoked them still
    So it's not that bad

--- migrations/2021-05-27-154211_account_changes_unique_idx/up.sql
+++ migrations/2021-05-27-154211_account_changes_unique_idx/up.sql
@@ -1,11 +1,11 @@
 CREATE UNIQUE INDEX account_changes_transaction_uni_idx
-ON account_changes (affected_account_id, changed_in_block_hash, caused_by_transaction_hash)
+ON account_changes (affected_account_id, changed_in_block_hash, caused_by_transaction_hash, update_reason)
 WHERE caused_by_transaction_hash IS NOT NULL AND caused_by_receipt_id IS NULL;
 
 CREATE UNIQUE INDEX account_changes_receipt_uni_idx
-ON account_changes (affected_account_id, changed_in_block_hash, caused_by_receipt_id)
+ON account_changes (affected_account_id, changed_in_block_hash, caused_by_receipt_id, update_reason)
 WHERE caused_by_transaction_hash IS NULL AND caused_by_receipt_id IS NOT NULL;
 
 CREATE UNIQUE INDEX account_changes_null_uni_idx
-ON account_changes (affected_account_id, changed_in_block_hash)
+ON account_changes (affected_account_id, changed_in_block_hash, update_reason)
 WHERE caused_by_transaction_hash IS NULL AND caused_by_receipt_id IS NULL;
