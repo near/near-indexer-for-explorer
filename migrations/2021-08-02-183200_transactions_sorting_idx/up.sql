71dbfb5e40077f3560e136f531164af8d369492c
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2021-08-04 11:38:56 +0300

    fix: remove CONCURRENTLY from migration (#141)
    CREATE INDEX CONCURRENTLY cannot run inside a transaction block

--- migrations/2021-08-02-183200_transactions_sorting_idx/up.sql
+++ migrations/2021-08-02-183200_transactions_sorting_idx/up.sql
@@ -1,1 +1,1 @@
-CREATE INDEX CONCURRENTLY transactions_sorting_idx ON transactions (block_timestamp, index_in_chunk);
+CREATE INDEX transactions_sorting_idx ON transactions (block_timestamp, index_in_chunk);
