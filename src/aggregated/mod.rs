133848b98aa7cfd9cc9aa2eda7364ad762148677
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2021-08-11 15:25:50 +0300

    fix: cleanup in circulating supply
    move run_circulating_supply_computation to mod rs
    prepare the place for other aggregated modules
    get rid of cargo clippy warnings

--- src/aggregated/mod.rs
+++ src/aggregated/mod.rs
@@ -1,2 +1,16 @@
-pub(crate) mod account_details;
-pub(crate) mod circulating_supply;
+use actix_diesel::Database;
+use diesel::PgConnection;
+use near_indexer::Indexer;
+
+mod account_details;
+mod circulating_supply;
+
+pub(crate) fn spawn_aggregated_computations(pool: Database<PgConnection>, indexer: &Indexer) {
+    let view_client = indexer.client_actors().0;
+    if indexer.near_config().genesis.config.chain_id == "mainnet" {
+        actix::spawn(circulating_supply::run_circulating_supply_computation(
+            view_client,
+            pool,
+        ));
+    }
+}
