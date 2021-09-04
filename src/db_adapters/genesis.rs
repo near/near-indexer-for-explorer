d2973f2f9ddac65f761221834d052a515535d045
Vlad Frolov <frolvlad@gmail.com>
2021-09-04 22:09:53 +0300

    hacked together a working version of `block_on` for the store_genesis_records

--- src/db_adapters/genesis.rs
+++ src/db_adapters/genesis.rs
@@ -4,6 +4,37 @@ use diesel::PgConnection;
 use crate::db_adapters::access_keys::store_access_keys_from_genesis;
 use crate::db_adapters::accounts::store_accounts_from_genesis;
 
+/// This is an ugly hack that allows to execute an async body on a specified actix runtime.
+/// You should only call it from a separate thread!
+///
+/// ```ignore
+/// async fn some_async_function() {
+///     let current_actix_system = actix::System::current();
+///     tokio::tasks::spawn_blocking(move || {
+///         let x = vec![0, 1, 2];
+///         x.map(|i| {
+///             block_on(current_actix_system, async move {
+///                 reqwest::get(...).await.text().await
+///             })
+///         });
+///     }
+/// }
+fn block_on<Fut, T>(
+    actix_arbiter: &actix_rt::ArbiterHandle,
+    f: Fut,
+) -> Result<T, std::sync::mpsc::RecvError>
+where
+    T: Send + 'static,
+    Fut: std::future::Future<Output = T> + Send + 'static,
+{
+    let (tx, rx) = std::sync::mpsc::channel();
+    actix_arbiter.spawn(async move {
+        let result = f.await;
+        let _ = tx.send(result);
+    });
+    rx.recv()
+}
+
 /// Iterates over GenesisRecords and stores selected ones (Accounts, AccessKeys)
 /// to database.
 /// Separately stores records divided in portions by 5000 to optimize
@@ -16,63 +47,73 @@ pub(crate) async fn store_genesis_records(
         target: crate::INDEXER_FOR_EXPLORER,
         "Storing genesis records to database...",
     );
-    let tokio_runtime = tokio::runtime::Handle::current();
     let genesis_height = near_config.genesis.config.genesis_height;
 
-    let mut accounts_to_store: Vec<crate::models::accounts::Account> = vec![];
-    let mut access_keys_to_store: Vec<crate::models::access_keys::AccessKey> = vec![];
+    // Remember the current actix runtime thread in order to be able to
+    // schedule async function on it from the thread that processes genesis in
+    // a blocking way.
+    let actix_system = actix::System::current();
+    // Spawn the blocking genesis processing on a separate thread
+    tokio::task::spawn_blocking(move || {
+        let actix_arbiter = actix_system.arbiter();
 
-    near_config.genesis.for_each_record(|record| {
-        if accounts_to_store.len() == 5_000 {
-            let mut accounts_to_store_chunk = vec![];
-            std::mem::swap(&mut accounts_to_store, &mut accounts_to_store_chunk);
-            let pool_copy = pool.clone();
-            // This will block current thread but there is no other async tasks
-            // running at the moment so it is fine to cut this corner for now
-            tokio_runtime.block_on(async move {
-                store_accounts_from_genesis(pool_copy, accounts_to_store_chunk).await;
-            });
-        }
-        if access_keys_to_store.len() == 5_000 {
-            let mut access_keys_to_store_chunk = vec![];
-            std::mem::swap(&mut access_keys_to_store, &mut access_keys_to_store_chunk);
-            let pool_copy = pool.clone();
-            // This will block current thread but there is no other async tasks
-            // running at the moment so it is fine to cut this corner for now
-            tokio_runtime.block_on(async move {
-                store_access_keys_from_genesis(pool_copy, access_keys_to_store_chunk).await;
-            });
-        }
+        let mut accounts_to_store: Vec<crate::models::accounts::Account> = vec![];
+        let mut access_keys_to_store: Vec<crate::models::access_keys::AccessKey> = vec![];
 
-        match record {
-            near_indexer::near_primitives::state_record::StateRecord::Account {
-                account_id,
-                ..
-            } => {
-                accounts_to_store.push(crate::models::accounts::Account::new_from_genesis(
-                    &account_id,
-                    genesis_height,
-                ));
+        near_config.genesis.for_each_record(|record| {
+            if accounts_to_store.len() == 5_000 {
+                let mut accounts_to_store_chunk = vec![];
+                std::mem::swap(&mut accounts_to_store, &mut accounts_to_store_chunk);
+                let pool = pool.clone();
+                block_on(actix_arbiter, async move {
+                    store_accounts_from_genesis(pool, accounts_to_store_chunk).await;
+                })
+                .expect("storing accounts from genesis failed");
             }
-            near_indexer::near_primitives::state_record::StateRecord::AccessKey {
-                account_id,
-                public_key,
-                access_key,
-            } => {
-                access_keys_to_store.push(crate::models::access_keys::AccessKey::from_genesis(
-                    &public_key,
-                    &account_id,
-                    &access_key,
-                    genesis_height,
-                ));
+            if access_keys_to_store.len() == 5_000 {
+                let mut access_keys_to_store_chunk = vec![];
+                std::mem::swap(&mut access_keys_to_store, &mut access_keys_to_store_chunk);
+                let pool = pool.clone();
+                block_on(actix_arbiter, async move {
+                    store_access_keys_from_genesis(pool, access_keys_to_store_chunk).await;
+                })
+                .expect("storing access keys from genesis failed");
             }
-            _ => {}
-        };
-    });
 
-    // Store leftovers vectors if their sizes are less than 5_000
-    store_accounts_from_genesis(pool.clone(), accounts_to_store).await;
-    store_access_keys_from_genesis(pool, access_keys_to_store).await;
+            match record {
+                near_indexer::near_primitives::state_record::StateRecord::Account {
+                    account_id,
+                    ..
+                } => {
+                    accounts_to_store.push(crate::models::accounts::Account::new_from_genesis(
+                        &account_id,
+                        genesis_height,
+                    ));
+                }
+                near_indexer::near_primitives::state_record::StateRecord::AccessKey {
+                    account_id,
+                    public_key,
+                    access_key,
+                } => {
+                    access_keys_to_store.push(crate::models::access_keys::AccessKey::from_genesis(
+                        &public_key,
+                        &account_id,
+                        &access_key,
+                        genesis_height,
+                    ));
+                }
+                _ => {}
+            };
+        });
+        block_on(actix_arbiter, async move {
+            // Store leftovers vectors if their sizes are less than 5_000
+            store_accounts_from_genesis(pool.clone(), accounts_to_store).await;
+            store_access_keys_from_genesis(pool, access_keys_to_store).await;
+        })
+        .expect("storing leftover accounts and access keys from genesis failed");
+    })
+    .await
+    .expect("storing genesis records failed");
 
     tracing::info!(
         target: crate::INDEXER_FOR_EXPLORER,
