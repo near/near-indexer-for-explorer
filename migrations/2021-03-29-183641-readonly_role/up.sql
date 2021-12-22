5081f394bb79217bacac13ea55a1b90ab59d5da2
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2021-12-22 18:16:21 +0300

    fix(migrations): remove migration with creating readonly role (#217)
    Move this info to README.md

--- migrations/2021-03-29-183641-readonly_role/up.sql
+++
@@ -1,9 +0,0 @@
-DO $$
-BEGIN
-CREATE ROLE readonly;
-EXCEPTION WHEN DUPLICATE_OBJECT THEN
-  RAISE NOTICE 'not creating role readonly -- it already exists';
-END
-$$;
-GRANT USAGE ON SCHEMA public TO readonly;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
