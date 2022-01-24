ca4bac559f3837c8e8ba25d1166df9e9d68b69f5
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2022-01-24 10:48:02 +0300

    fix(migrations): remove all appearances of readonly

--- migrations/2021-06-09-102523_grant_select_on_new_tables/down.sql
+++
@@ -1,3 +0,0 @@
--- It does not affect privileges assigned to already-existing objects.
--- Source: https://www.postgresql.org/docs/current/sql-alterdefaultprivileges.html
-ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE SELECT ON TABLES FROM readonly;
