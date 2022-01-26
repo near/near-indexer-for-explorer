36eec27ebd31950458db59c5f278c5bf4108847d
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2022-01-26 19:21:39 +0300

    drop concurrently

--- migrations/2022-01-26-184200_drop_duplicated_index_execution_outcome/down.sql
+++ migrations/2022-01-26-184200_drop_duplicated_index_execution_outcome/down.sql
@@ -1,3 +1,3 @@
 -- I suggest to leave it blank because this index duplicates PK index created automatically
 -- If we really need to re-create it, it should be done by:
--- CREATE INDEX CONCURRENTLY execution_outcomes_receipt_id_idx ON execution_outcomes (receipt_id);
+-- CREATE INDEX execution_outcomes_receipt_id_idx ON execution_outcomes (receipt_id);
