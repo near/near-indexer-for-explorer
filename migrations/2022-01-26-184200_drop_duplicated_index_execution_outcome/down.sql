31e6d3c6930bf65f998284d3ba5dc6fd73d08ef6
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2022-01-26 19:22:12 +0300

    Drop duplicated index, add index on action_receipt_actions (#243)
    * fix(migrations): drop duplicated index
    
    * fix(migrations): add index on action_receipt_actions

---
+++ migrations/2022-01-26-184200_drop_duplicated_index_execution_outcome/down.sql
@@ -0,0 +1,3 @@
+-- I suggest to leave it blank because this index duplicates PK index created automatically
+-- If we really need to re-create it, it should be done by:
+-- CREATE INDEX execution_outcomes_receipt_id_idx ON execution_outcomes (receipt_id);
