2d66461993a867f6611beab6f567593808dedee3
Morgan McCauley <morgan@mccauley.co.nz>
2022-10-14 06:48:08 +1300

    fix: Avoid recreating access key on transfer to implicit account (#317)
    * fix: Avoid recreating access key on transfer to implicit account
    
    * refactor: Remove `AccountDeletion` logic as its also covered by `AccessKeyDeletion`
    
    * chore: Update `CHANGELOG.md`

--- src/db_adapters/access_keys.rs
+++ src/db_adapters/access_keys.rs
@@ -1,9 +1,7 @@
 use std::collections::HashMap;
-use std::convert::TryFrom;
 
 use actix_diesel::dsl::AsyncRunQueryDsl;
 use actix_diesel::Database;
-use bigdecimal::BigDecimal;
 use diesel::{ExpressionMethods, PgConnection, QueryDsl};
 use futures::try_join;
 use tracing::info;
@@ -15,107 +13,61 @@ use crate::{metrics, models};
 
 pub(crate) async fn handle_access_keys(
     pool: &actix_diesel::Database<PgConnection>,
-    outcomes: &[near_indexer::IndexerExecutionOutcomeWithReceipt],
+    state_changes: &[near_primitives::views::StateChangeWithCauseView],
     block_height: near_primitives::types::BlockHeight,
 ) -> anyhow::Result<()> {
     let _timer = metrics::STORE_TIME
         .with_label_values(&["AccessKeys"])
         .start_timer();
-    if outcomes.is_empty() {
+    if state_changes.is_empty() {
         return Ok(());
     }
-    let successful_receipts = outcomes
-        .iter()
-        .filter(|outcome_with_receipt| {
-            matches!(
-                outcome_with_receipt.execution_outcome.outcome.status,
-                near_primitives::views::ExecutionStatusView::SuccessValue(_)
-                    | near_primitives::views::ExecutionStatusView::SuccessReceiptId(_)
-            )
-        })
-        .map(|outcome_with_receipt| &outcome_with_receipt.receipt);
 
     let mut access_keys = HashMap::<(String, String), models::access_keys::AccessKey>::new();
-    let mut deleted_accounts = HashMap::<String, String>::new();
 
-    for receipt in successful_receipts {
-        if let near_primitives::views::ReceiptEnumView::Action { actions, .. } = &receipt.receipt {
-            for action in actions {
-                match action {
-                    near_primitives::views::ActionView::DeleteAccount { .. } => {
-                        deleted_accounts.insert(
-                            receipt.receiver_id.to_string(),
-                            receipt.receipt_id.to_string(),
-                        );
-                        access_keys
-                            .iter_mut()
-                            .filter(|((_, receiver_id), _)| {
-                                receiver_id == receipt.receiver_id.as_ref()
-                            })
-                            .for_each(|(_, access_key)| {
-                                access_key.deleted_by_receipt_id =
-                                    Some(receipt.receipt_id.to_string());
-                            });
-                    }
-                    near_primitives::views::ActionView::AddKey {
-                        public_key,
-                        access_key,
-                    } => {
-                        access_keys.insert(
-                            (public_key.to_string(), receipt.receiver_id.to_string()),
-                            models::access_keys::AccessKey::from_action_view(
-                                public_key,
-                                &receipt.receiver_id,
-                                access_key,
-                                &receipt.receipt_id,
-                                block_height,
-                            ),
-                        );
-                    }
-                    near_primitives::views::ActionView::DeleteKey { public_key } => {
-                        access_keys
-                            .entry((public_key.to_string(), receipt.receiver_id.to_string()))
-                            .and_modify(|existing_access_key| {
-                                existing_access_key.deleted_by_receipt_id =
-                                    Some(receipt.receipt_id.to_string());
-                            })
-                            .or_insert_with(|| models::access_keys::AccessKey {
-                                public_key: public_key.to_string(),
-                                account_id: receipt.receiver_id.to_string(),
-                                created_by_receipt_id: None,
-                                deleted_by_receipt_id: Some(receipt.receipt_id.to_string()),
-                                // this is a workaround to avoid additional struct with optional field
-                                // permission_kind is not supposed to change on delete action
-                                permission_kind: models::enums::AccessKeyPermission::FullAccess,
-                                last_update_block_height: block_height.into(),
-                            });
-                    }
-                    near_indexer::near_primitives::views::ActionView::Transfer { .. } => {
-                        if receipt.receiver_id.len() != 64usize {
-                            continue;
-                        }
-                        if let Ok(public_key_bytes) = hex::decode(receipt.receiver_id.as_ref()) {
-                            if let Ok(public_key) =
-                                near_crypto::ED25519PublicKey::try_from(&public_key_bytes[..])
-                            {
-                                access_keys.insert(
-                                    (near_crypto::PublicKey::from(public_key.clone()).to_string(), receipt.receiver_id.to_string()),
-                                    models::access_keys::AccessKey::from_action_view(
-                                        &near_crypto::PublicKey::from(public_key.clone()),
-                                        &receipt.receiver_id,
-                                        &near_primitives::views::AccessKeyView {
-                                            nonce: 0,
-                                            permission: near_primitives::views::AccessKeyPermissionView::FullAccess
-                                        },
-                                        &receipt.receipt_id,
-                                        block_height,
-                                    ),
-                                );
-                            }
-                        }
-                    }
-                    _ => continue,
+    for state_change in state_changes {
+        if let near_primitives::views::StateChangeCauseView::ReceiptProcessing { receipt_hash } =
+            state_change.cause
+        {
+            match &state_change.value {
+                near_primitives::views::StateChangeValueView::AccessKeyUpdate {
+                    account_id,
+                    public_key,
+                    access_key,
+                } => {
+                    access_keys.insert(
+                        (public_key.to_string(), account_id.to_string()),
+                        models::access_keys::AccessKey::from_action_view(
+                            public_key,
+                            &account_id,
+                            access_key,
+                            &receipt_hash,
+                            block_height,
+                        ),
+                    );
                 }
+                near_primitives::views::StateChangeValueView::AccessKeyDeletion {
+                    account_id,
+                    public_key,
+                } => {
+                    access_keys
+                        .entry((public_key.to_string(), account_id.to_string()))
+                        .and_modify(|existing_access_key| {
+                            existing_access_key.deleted_by_receipt_id =
+                                Some(receipt_hash.to_string());
+                        })
+                        .or_insert_with(|| models::access_keys::AccessKey {
+                            public_key: public_key.to_string(),
+                            account_id: account_id.to_string(),
+                            created_by_receipt_id: None,
+                            deleted_by_receipt_id: Some(receipt_hash.to_string()),
+                            // this is a workaround to avoid additional struct with optional field
+                            // permission_kind is not supposed to change on delete action
+                            permission_kind: models::enums::AccessKeyPermission::FullAccess,
+                            last_update_block_height: block_height.into(),
+                        });
+                }
+                _ => continue,
             }
         }
     }
@@ -127,34 +79,6 @@ pub(crate) async fn handle_access_keys(
         .values()
         .cloned()
         .partition(|model| model.created_by_receipt_id.is_some());
-
-    let delete_access_keys_for_deleted_accounts = async {
-        let last_update_block_height: BigDecimal = block_height.into();
-        for (account_id, deleted_by_receipt_id) in deleted_accounts {
-            let target = schema::access_keys::table
-                .filter(schema::access_keys::dsl::deleted_by_receipt_id.is_null())
-                .filter(
-                    schema::access_keys::dsl::last_update_block_height
-                        .lt(last_update_block_height.clone()),
-                )
-                .filter(schema::access_keys::dsl::account_id.eq(account_id));
-
-            crate::await_retry_or_panic!(
-                diesel::update(target.clone())
-                    .set((
-                        schema::access_keys::dsl::deleted_by_receipt_id
-                            .eq(deleted_by_receipt_id.clone()),
-                        schema::access_keys::dsl::last_update_block_height
-                            .eq(last_update_block_height.clone()),
-                    ))
-                    .execute_async(pool),
-                10,
-                "AccessKeys were deleting".to_string(),
-                &deleted_by_receipt_id
-            );
-        }
-        Ok(())
-    };
 
     let update_access_keys_future = async {
         for value in access_keys_to_update {
@@ -222,11 +146,7 @@ pub(crate) async fn handle_access_keys(
         Ok(())
     };
 
-    try_join!(
-        delete_access_keys_for_deleted_accounts,
-        update_access_keys_future,
-        add_access_keys_future
-    )?;
+    try_join!(update_access_keys_future, add_access_keys_future)?;
 
     Ok(())
 }
